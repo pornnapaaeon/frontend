e68aed045bfa8cd15c3ff0f80c9b563fad12bfa0
pornnapa khiao-asawa <banma3475@gmail.com>
2021-03-25 16:13:38 +0800

    init project

--- pages/menu.js
+++ pages/menu.js
@@ -7,33 +7,10 @@ export default function Home() {
   <div>
     <AdminHeader />
     <AdminSiderbar />
-{/*     <div className="content-wrapper text-center">
-    <h1>Hello world</h1>
-    <br />
-    <h1>My name is</h1>
-    <br />
-    <h2>worachet raphukhiew</h2>
-    </div> */}
-<div className="content-wrapper">
-  {/* Content Header (Page header) */}
-  <div className="content-header">
-    <div className="container-fluid">
-      <div className="row mb-2">
-        <div className="col-sm-6">
-          <h1 className="m-0">Dashboard v3</h1>
-        </div>{/* /.col */}
-        <div className="col-sm-6">
-          <ol className="breadcrumb float-sm-right">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
-            <li className="breadcrumb-item active">Dashboard v3</li>
-          </ol>
-        </div>{/* /.col */}
-      </div>{/* /.row */}
-    </div>{/* /.container-fluid */}
-  </div>
-  <div className="content">
-    <a> MENU pornnapa 052</a>
-</div>
+    <div>
+        <a>MENU</a>
+    </div>
+
 
 <AdminFooter />
     </div>
