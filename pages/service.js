76affc824be7e2b3351ca870c11a8dbe0f9cecef
pornnapa khiao-asawa <banma3475@gmail.com>
2021-03-25 16:09:14 +0800

    init project

--- pages/service.js
+++ pages/service.js
@@ -14,10 +14,26 @@ export default function Home() {
     <br />
     <h2>worachet raphukhiew</h2>
     </div> */}
-    <div>
-        <h1>เกี่ยวกับ พรนภา</h1>
-    </div>
-
+<div className="content-wrapper">
+  {/* Content Header (Page header) */}
+  <div className="content-header">
+    <div className="container-fluid">
+      <div className="row mb-2">
+        <div className="col-sm-6">
+          <h1 className="m-0">Dashboard v3</h1>
+        </div>{/* /.col */}
+        <div className="col-sm-6">
+          <ol className="breadcrumb float-sm-right">
+            <li className="breadcrumb-item"><a href="#">Home</a></li>
+            <li className="breadcrumb-item active">Dashboard v3</li>
+          </ol>
+        </div>{/* /.col */}
+      </div>{/* /.row */}
+    </div>{/* /.container-fluid */}
+  </div>
+  <div className="content">
+    <a> SERVICE pornnapa 052</a>
+</div>
 
 <AdminFooter />
     </div>
