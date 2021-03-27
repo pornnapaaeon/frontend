5b90ced873f8dcd2eee51745e8cb7dd196d709d7
pornnapa khiao-asawa <banma3475@gmail.com>
2021-03-27 10:37:08 +0800

    init project

--- pages/add.js
+++ pages/add.js
@@ -35,25 +35,33 @@ export default function Home() {
 
 
 {/* /.content */}
-<div classname="container-fluid">
-  <form>
-    <div classname="form-group">
-      <label htmlfor="exampleInputEmail1">Email address</label>
-      <input type="email" classname="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-      <small id="emailHelp" classname="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div classname="form-group">
-      <label htmlfor="exampleInputPassword1">Password</label>
-      <input type="password" classname="form-control" id="exampleInputPassword1" placeholder="Password" />
-    </div>
-    <div classname="form-group form-check">
-      <input type="checkbox" classname="form-check-input" id="exampleCheck1" />
-      <label classname="form-check-label" htmlfor="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" classname="btn btn-primary">Submit</button>
-  </form>
-</div>
+<div className="container-fluid">
 
+
+
+<form action="/action_page.php">
+  <div className="form-group">
+    <label htmlFor="email">Email address:</label>
+    <input type="email" className="form-control" id="email" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="pwd">Password:</label>
+    <input type="password" className="form-control" id="pwd" />
+  </div>
+  <div className="checkbox">
+    <label><input type="checkbox" /> Remember me</label>
+  </div>
+  <button type="submit" className="btn btn-default">Submit</button>
+</form>
+
+
+
+
+
+
+    
+
+</div>
 
 
 
