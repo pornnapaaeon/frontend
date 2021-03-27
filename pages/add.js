65c764489159928332ca3f188911f037c8879ca5
pornnapa khiao-asawa <banma3475@gmail.com>
2021-03-27 10:19:38 +0800

    init project

--- pages/add.js
+++ pages/add.js
@@ -37,7 +37,53 @@ export default function Home() {
 {/* /.content */}
 <div className="container-fluid">
 
-<a>พรนภา</a>
+
+<form>
+  <div className="form-row">
+    <div className="col-md-4 mb-3">
+      <label htmlFor="validationServer01">First name</label>
+      <input type="text" className="form-control is-valid" id="validationServer01" placeholder="First name" defaultValue="Mark" required />
+      <div className="valid-feedback">
+        Looks good!
+      </div>
+    </div>
+    <div className="col-md-4 mb-3">
+      <label htmlFor="validationServer02">Last name</label>
+      <input type="text" className="form-control is-valid" id="validationServer02" placeholder="Last name" defaultValue="Otto" required />
+      <div className="valid-feedback">
+        Looks good!
+      </div>
+    </div>
+    <div className="col-md-4 mb-3">
+      <label htmlFor="validationServerUsername">Username</label>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroupPrepend3">@</span>
+        </div>
+        <input type="text" className="form-control is-invalid" id="validationServerUsername" placeholder="Username" aria-describedby="inputGroupPrepend3" required />
+        <div className="invalid-feedback">
+          Please choose a username.
+        </div>
+      </div>
+    </div>
+    <div className="col-md-4 mb-3">
+      <label htmlFor="validationServer01">Password</label>
+      <input type="text" className="form-control is-valid" id="validationServer01" placeholder="password" defaultValue="Mark" required />
+      <div className="valid-feedback">
+        Looks good!
+      </div>
+    </div>
+  </div>
+  <button className="btn btn-primary" type="submit">Submit</button>
+</form>
+
+
+
+
+
+
+    
+
 </div>
 
 
